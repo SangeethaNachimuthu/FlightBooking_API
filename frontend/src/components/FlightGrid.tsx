35a7008f49c6037d907e19ace6b29ae90b69429f
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2026-05-15 13:33:54 +0200

    Adding Pagination section

--- frontend/src/components/FlightGrid.tsx
+++ frontend/src/components/FlightGrid.tsx
@@ -1,16 +1,83 @@
 import type {FlightItem} from "../types/flight.ts";
 import FlightCard from "./FlightCard.tsx";
+import {useState} from "react";
+import {ChevronLeft, ChevronRight} from "lucide-react";
 
 type Props = {
     flights: FlightItem[];
 }
 
 const FlightGrid = ({flights} : Props) => {
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const flightsPerPage = 6;
+    const startIndex = (currentPage - 1) * flightsPerPage;
+    const currentFlights = flights.slice(startIndex, startIndex + flightsPerPage);
+    const totalPages = Math.ceil(flights.length / flightsPerPage);
+
     return (
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-5">
-            {flights.map((flight) => (
-                <FlightCard key={flight.id} flight={flight} />
-            ))}
+        <div className="mt-5">
+
+            {/* Flight cards grid */}
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                {currentFlights.map((flight) => (
+                    <FlightCard key={flight.id} flight={flight} />
+                ))}
+            </div>
+
+            {/* Pagination */}
+            <div className="mt-8 flex items-center justify-center gap-2">
+
+                {/* Previous */}
+                <button
+                    onClick={() =>
+                        setCurrentPage((prev) =>
+                            Math.max(prev - 1, 1)
+                        )
+                    }
+                    disabled={currentPage === 1}
+                    className="inline-flex h-9 w-9 items-center justify-center font-medium rounded-lg border border-slate-200 hover:border-blue-300 hover:bg-blue-50 disabled:opacity-40 disabled:cursor-not-allowed"
+                >
+                    <ChevronLeft size={20} />
+                </button>
+
+                {/* Page numbers */}
+                {Array.from({ length: Math.min(5, totalPages) }, (_, index) => {
+
+                    const startPage = Math.max(
+                        Math.min(currentPage - 2, totalPages - 4), 1
+                    );
+
+                    const page = startPage + index;
+
+                    return (
+                        <button
+                            key={page}
+                            onClick={() => setCurrentPage(page)}
+                            className={`h-9 w-9 rounded-lg border transition 
+                                    ${currentPage === page
+                                ? "bg-blue-600 text-white border-blue-600"
+                                : "border-slate-200 hover:border-blue-300"}
+                                `}
+                        >
+                            {page}
+                        </button>
+                    );
+                })}
+
+                {/* Next */}
+                <button
+                    onClick={() =>
+                        setCurrentPage((prev) =>
+                            Math.min(prev + 1, totalPages)
+                        )
+                    }
+                    disabled={currentPage === totalPages}
+                    className="inline-flex h-9 w-9 items-center justify-center font-medium rounded-lg border border-slate-200 hover:border-blue-300 hover:bg-blue-50 disabled:opacity-40 disabled:cursor-not-allowed"
+                >
+                    <ChevronRight size={20} className="items-center" />
+                </button>
+            </div>
         </div>
     );
 };
